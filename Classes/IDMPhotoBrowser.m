eecde393e5e73485a3084091cb7c27e517d2e6be
adriaan <adriaan@infoding.com>
2015-04-30 17:34:09 +0200

    Made delegate callback post-sharing optional

--- Classes/IDMPhotoBrowser.m
+++ Classes/IDMPhotoBrowser.m
@@ -1259,7 +1259,9 @@ NSLocalizedStringFromTableInBundle((key), nil, [NSBundle bundleWithPath:[[NSBund
             [self.activityViewController setCompletionHandler:^(NSString *activityType, BOOL completed) {
                 [selfBlock hideControlsAfterDelay];
                 selfBlock.activityViewController = nil;
-                [selfBlock.delegate photoBrowser:selfBlock didFinishSharingWithActivityType:activityType completed:completed];
+                if(selfBlock.delegate && [selfBlock.delegate respondsToSelector:@selector(photoBrowser:didFinishSharingWithActivityType:completed:)]){
+                    [selfBlock.delegate photoBrowser:selfBlock didFinishSharingWithActivityType:activityType completed:completed];
+                }
             }];
             
             [self presentViewController:self.activityViewController animated:YES completion:nil];
